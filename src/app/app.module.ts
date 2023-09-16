4f218147e2b2f1e6b6ce9d7c452f86392c580b6a
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-16 14:26:26 -0300

    setup already

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MomentFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
