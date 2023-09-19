4a5dc48c95c6bab6eb71a91be304c03b3d5296a9
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-19 10:42:42 -0300

    fnished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CampeonatoService } from './services/campeonato.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'certificado-angular';
+
+export class AppComponent implements OnInit {
+  tabela: any;
+  ngOnInit(): void {
+    this.tabela = this.campeonatoService.getTabelaSerieB();
+  }
+
+  constructor(private campeonatoService: CampeonatoService){
+
+  }
 }
